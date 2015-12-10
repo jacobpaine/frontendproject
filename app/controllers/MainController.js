8ed3e507ca621bfd47049eaa744aaed5fd48a72c
jacobpaine <painejacob@gmail.com>
2015-12-10 12:17:05 -0600

    Authentication occurs. User populates to FB

---
+++ app/controllers/MainController.js
@@ -0,0 +1,9 @@
+
+
+app.controller("MainController",
+  ["$q", "$scope", "$firebaseAuth",
+  function($Q, $scope, $firebaseAuth) {
+
+
+
+}]);
---
+++ app/factories/Auth.js
@@ -0,0 +1,27 @@
+// let's create a re-usable factory that generates the $firebaseAuth instance
+// It's a factory! Inside the factory are all the tools we want to call in other places. This one uses Firebase information, e.g. $firebaseAuth.  
+
+app.factory("Auth", 
+	["$firebaseAuth", function($firebaseAuth) {
+		var ref = new Firebase("https://front-end-data.firebaseio.com");
+    var uid;
+
+	return {
+    getUid: function(){
+      return uid;
+    },
+    setUid: function(passedUid){
+      uid = passedUid;
+    },
+		useAuth: function() {
+    return $firebaseAuth(ref);
+  	},
+  // 	logUs: function(inorOut){
+  // 		$rootScope.loggedIn = inorOut;
+  // 		console.log("inorOut", inorOut);
+  // 	},
+  	isLoggedIn: function(){
+  		return $rootScope.loggedIn;
+  	}
+  };
+}]);
